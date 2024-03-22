9c2e77c4e994130be407ef845abec6818ee7b3e6
PaulTrusso <pault1433@gmail.com>
2024-03-22 11:52:02 -0500

    final changes

--- grandpaap/src/components/welcome.jsx
+++ grandpaap/src/components/welcome.jsx
@@ -1,20 +1,48 @@
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
 
 const WelcomePage = () => {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', height: '100vh', width: '100vw', margin: '0', padding: '0' }}>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        height: "100vh",
+        width: "100vw",
+        margin: "0",
+        padding: "0",
+      }}
+    >
       {/* Buttons */}
-      <div style={{ flex: '3', display: 'flex', justifyContent: 'space-around', alignItems: 'center', width: '100%', marginTop: '20px' }}>
+      <div
+        style={{
+          flex: "3",
+          display: "flex",
+          justifyContent: "space-around",
+          alignItems: "center",
+          width: "100%",
+          marginTop: "20px",
+        }}
+      >
         {/* Adjusted marginTop here */}
-        <Button variant="outlined" fullWidth  style={{ height: '80%', fontSize: '40px' }} href="/group">
+        <Button
+          variant="outlined"
+          fullWidth
+          style={{ height: "80%", fontSize: "40px" }}
+          href="/group"
+        >
           Group
         </Button>
-        <Button variant="outlined" fullWidth  style={{ height: '80%', fontSize: '40px' }} href="/message">
+        <Button
+          variant="outlined"
+          fullWidth
+          style={{ height: "80%", fontSize: "40px" }}
+          href="/message"
+        >
           Mail
         </Button>
       </div>
     </div>
   );
-}
+};
 
 export default WelcomePage;
