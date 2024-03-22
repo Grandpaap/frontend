0699e13060f58feaa9fa21ee69dfdfaec0233d11
PaulTrusso <pault1433@gmail.com>
2024-03-22 12:02:21 -0500

    deleting old files

--- grandpaap/src/App.jsx
+++ grandpaap/src/App.jsx
@@ -3,8 +3,6 @@ import { Routes, Route } from "react-router-dom";
 import Register from './components/register';
 import Login from './components/login';
 import Message from './components/message';
-import Photo from './components/photo';
-import GroupMessage from './components/groupmessage';
 import WelcomePage from './components/welcome';
 import Group from './components/group';
 import GroupSignup from './components/groupsignup';
@@ -17,8 +15,6 @@ function App() {
       <Route path = "/groupsignup" element = {<GroupSignup/>} />
         <Route path = "/group" element = {<Group/>} />
         <Route path = "/" element = {<WelcomePage/>} />
-        <Route path = "/groupMessage" element = {<GroupMessage/>} />
-        <Route path = "/photo" element = {<Photo/>} />
         <Route path = "/register" element = {<Register/>} />
         <Route path = "/login" element = {<Login/>} />
         <Route path = "/message" element = {<Message/>} />
